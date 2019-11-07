a46de8cbf1d39a8406d6194bb3c6b3b73e3dc3eb
Ryunseok Kim <ryunseok.kim@ringleplus.com>
2019-11-07 20:52:17 +0900

    added login page and style

--- src/sign/SignIn.js
+++ src/sign/SignIn.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Sign.css";
+import { thisExpression } from "@babel/types";
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -29,29 +30,64 @@ class SignIn extends React.Component {
 	}
 
 	submit() {
-		alert("submit clicked");
-
-		// alert(this.state.email);
-		// alert(this.state.password);
-
 		const { email, password } = this.state;
-		alert(email);
+		const payload = {
+			email: email,
+			password: password
+		};
+
+		const url =
+			"http://stagings.ringleplus.com/api/v3/common/authenticate/simple_signin";
+
+		const opts = {
+			method: "POST", //POST : request for editing info, GET: request for getting info
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(payload)
+		};
+
+		fetch(url, opts)
+			.then(res => res.json())
+			.then(
+				result => {
+					console.log(result);
+					if (result.success) {
+						alert("Success");
+					} else {
+						alert(result.message);
+					}
+				},
+				error => {
+					console.log(error);
+				}
+			);
 	}
 
 	render() {
 		return (
 			<div>
-				<input
-					value={this.state.email}
-					onChange={this.updateEmail}
-				></input>
-				<input
-					type="password"
-					value={this.state.password}
-					onChange={this.updatePassword}
-				></input>
-
-				<a onClick={this.submit}>Submit</a>
+				<div className="inputTagWrapper">
+					<label>Email</label>
+					<input
+						className="inputLabel"
+						value={this.state.email}
+						onChange={this.updateEmail}
+					></input>
+				</div>
+				<div className="inputTagWrapper">
+					<label>Password</label>
+					<input
+						className="inputLabel"
+						type="password"
+						value={this.state.password}
+						onChange={this.updatePassword}
+					></input>
+				</div>
+				<div className="btn-style">
+					<a onClick={this.submit}>Submit</a>
+				</div>
 			</div>
 		);
 	}
